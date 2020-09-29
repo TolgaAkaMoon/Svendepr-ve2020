66736d5b0e4a11cbfeff8c6a53786fd9e4e7bd61
TolgaAkaMoon <4160141@RTS-365.dk>
2020-09-29 13:16:49 +0200

    add ejs for all sub menus and added style to auth

--- src/javascript/auth.js
+++ src/javascript/auth.js
@@ -6,7 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
   button.addEventListener("click", function () {
     if (email.value !== "") {
       document.querySelector(".form__user--error").style.opacity = "0";
-      fetch("https://svende-api-tolga.herokuapp.com/auth/token", {
+      if (pass.value !== ""){
+        document.querySelector(".form__user--error").style.opacity = "0";
+      fetch("https://svende-api-tolga-fixed.herokuapp.com/auth/token", {
         "method": "POST",
         "headers": {
           "Content-Type": "application/x-www-form-urlencoded"
@@ -18,9 +20,22 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
           if (data.token !== undefined) {
             sessionStorage.setItem("token", data.token);
+            // sessionStorage.getItem("token")
+            document.querySelector(".form__pass--error").innerHTML = "Succssfully signed in"
+            document.querySelector(".form__pass--error").style.opacity = "1";
+            document.querySelector(".form__pass--error").style.color = "green";
+          } else if (data) {
+            document.querySelector(".form__pass--error").innerHTML = "Error, could not sign in"
+            document.querySelector(".form__pass--error").style.opacity = "1";
+            document.querySelector(".form__pass--error").style.color = "red";
           }
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          document.querySelector(".form__pass--error").innerHTML = "Wrong username or password! " 
+          document.querySelector(".form__pass--error").style.opacity = "1";
+          document.querySelector(".form__pass--error").style.color = "red";
+        });
+      }
     } else {
         document.querySelector(".form__user--error").innerHTML = "Username cannot be blank"
         document.querySelector(".form__user--error").style.opacity = "1";
