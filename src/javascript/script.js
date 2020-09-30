266f992fdff96f7b3ae1f84729a9922cbe9292a6
Tolga Özdemir <4160141@RTS-365.dk>
2020-09-30 13:54:50 +0200

    fetch from api on animals, about & volunteer page

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,12 +1,97 @@
 document.addEventListener("DOMContentLoaded", function () {
-        fetch("https://svende-api-tolga-fixed.herokuapp.com", {
-          "method": "get"
+
+    fetch("https://svende-api-tolga-fixed.herokuapp.com/api/v1/abouts", {
+        method: "get",
+      })
+        .then((response) => response.json())
+        .then((about) => {
+            console.log(about)
+            about.forEach(omos => {
+                document.querySelector(".about").innerHTML += `
+                
+                `
+            });
         })
-  
-          .then((response) => response.json())
-          .then((data) => {
-        })
-          .catch((err) => {
+        .catch((err) => {
+          console.log(err);
         });
-  });
-  
+
+
+// Volunteers Script
+  fetch("https://svende-api-tolga-fixed.herokuapp.com/api/v1/volunteers", {
+    method: "get",
+  })
+    .then((response) => response.json())
+    .then((voluns) => {
+      voluns.forEach((volunteer) => {
+        let volunteerUrl = volunteer.asset.url.replace(
+          "localhost:4000",
+          "svende-api-tolga-fixed.herokuapp.com"
+        );
+        if (volunteer.extra == "") {
+          document.querySelector(".frivilig__section").innerHTML += `
+                <div class="frivilig__box">
+                <div class="box__textFrivilig"><span class="textFrivilig__span">${volunteer.title}</span></div>
+                <div class="box__container">
+                    <img class="container__imgFrivilig" src="${volunteerUrl}">
+                    <p class="container__descFrivilig">${volunteer.content}</p>
+                    </div>
+                </div>
+                `;
+        } else {
+          document.querySelector(".frivilig__section").innerHTML += `
+                <div class="frivilig__box">
+                <div class="box__textFrivilig"><span class="textFrivilig__span">${volunteer.title}</span></div>
+                <div class="box__container">
+                    <img class="container__imgFrivilig" src="${volunteerUrl}">
+                    <p class="container__descFrivilig">${volunteer.content}</p>
+                    </div>
+                    <div class="box__bottomFrivilig"><span class="bottomFrivilig__span">${volunteer.extra}</span></div>
+                </div>
+                `;
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+
+    // Animals Script
+  fetch("https://svende-api-tolga-fixed.herokuapp.com/api/v1/animals", {
+    method: "get",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".box__countVores").innerHTML =
+        data.length + " " + "Dyr";
+      data.forEach((animal) => {
+        let idag = new Date();
+        let dageSiden = new Date(animal.createdAt);
+        dageSiden.setHours("0", "0", "0", "0");
+        idag.setHours("0", "0", "0", "0");
+        let forskellenTid = dageSiden.getTime() - idag.getTime();
+        let forskelleniDage = forskellenTid / (1000 * 3600 * 24);
+        let nyTid = `${forskelleniDage}`.replace("-", "");
+
+        let fixedUrl = animal.asset.url.replace(
+          "localhost:4000",
+          "svende-api-tolga-fixed.herokuapp.com"
+        );
+        let dateF = animal.createdAt;
+        document.querySelector(".vores__animals").innerHTML += `
+                <div class="animals__animal">
+                    <img class="animal__img" src="${fixedUrl}">
+                    <section>
+                        <h3 class="animal__title">${animal.name}</h3>
+                        <p class="animal__desc">${animal.description}</p>
+                        <p class="animal__time">Været på internettet i ${nyTid} dage.</p>
+                    </section>
+                </div>
+                `;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
