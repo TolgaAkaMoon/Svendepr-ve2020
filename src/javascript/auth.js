7bde1dc8de6a6a1d653b4d8f27f0ab4e960c4cc3
TolgaAkaMoon <4160141@rts-365.dk>
2020-10-05 11:28:38 +0200

    fix admin style and made it danish

--- src/javascript/auth.js
+++ src/javascript/auth.js
@@ -7,7 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     if (email.value !== "") {
       document.querySelector(".form__user--error").style.opacity = "0";
       if (pass.value !== ""){
-        document.querySelector(".form__user--error").style.opacity = "0";
+        document.querySelector(".form__pass--error").innerHTML = "Logger ind...."
+        document.querySelector(".form__pass--error").style.opacity = "1";
+        document.querySelector(".form__pass--error").style.color = "gray";
       fetch("https://svende-api-tolga-fixed.herokuapp.com/auth/token", {
         "method": "POST",
         "headers": {
@@ -21,29 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
           if (data.token !== undefined) {
             sessionStorage.setItem("token", data.token);
             // sessionStorage.getItem("token")
-            document.querySelector(".form__pass--error").innerHTML = "Succssfully signed in"
+            document.querySelector(".form__pass--error").innerHTML = "Du er nu logget ind :D"
             document.querySelector(".form__pass--error").style.opacity = "1";
-            document.querySelector(".form__pass--error").style.color = "green";
+            document.querySelector(".form__pass--error").style.color = "green"
           } else if (data) {
-            document.querySelector(".form__pass--error").innerHTML = "Error, could not sign in"
+            document.querySelector(".form__pass--error").innerHTML = "Fejl, kunne ikke logge ind!"
             document.querySelector(".form__pass--error").style.opacity = "1";
             document.querySelector(".form__pass--error").style.color = "red";
           }
         })
         .catch((err) => {
-          document.querySelector(".form__pass--error").innerHTML = "Wrong username or password! " 
+          document.querySelector(".form__pass--error").innerHTML = "Forkert brugernavn eller adgangskode!" 
           document.querySelector(".form__pass--error").style.opacity = "1";
           document.querySelector(".form__pass--error").style.color = "red";
         });
       }
     } else {
-        document.querySelector(".form__user--error").innerHTML = "Username cannot be blank"
+        document.querySelector(".form__user--error").innerHTML = "Brugernavn må ikke være tomt"
         document.querySelector(".form__user--error").style.opacity = "1";
     };
     if (pass.value !== "") {
-        document.querySelector(".form__pass--error").style.opacity = "0";
     } else {
-        document.querySelector(".form__pass--error").innerHTML = "Password cannot be blank"
+        document.querySelector(".form__pass--error").innerHTML = "Adgangskode må ikke være tomt"
         document.querySelector(".form__pass--error").style.opacity = "1";
     };
   });
